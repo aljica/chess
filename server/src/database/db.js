385718eafba40c6e46df71b21f83a4fe539aa3b2
aljica <aljica@kth.se>
2021-04-01 10:19:45 +0200

    add try-catch

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -10,6 +10,12 @@ exports.insertNewChessGame = () => {
   const gameID = Math.random();
   const startingFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'; // Starting chess position
   db.prepare('INSERT INTO games VALUES(?, ?, ?, ?, ?, ?)').run(gameID, null, null, startingFEN, '', 0);
+  try {
+    db.prepare('INSERT INTO games VALUES(?, ?, ?, ?, ?, ?)').run(gameID, null, null, startingFEN, '', 0);
+  } catch(e) {
+    return false; // In case gameID already exists (highly unlikely!).
+  }
+  
   return gameID;
 };
 
