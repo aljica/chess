34e8b156a6a1bb945020fcc9ef7a47c620dd501f
aljica <aljica@kth.se>
2021-04-14 14:36:57 +0200

    refactor controller

--- server/src/controllers/game.controller.js
+++ server/src/controllers/game.controller.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const model = require("../model.js");
+const express = require('express');
+const model = require('../model.js');
 
 const router = express.Router();
 
-router.get("/gameList", (req, res) => {
+router.get('/gameList', (req, res) => {
   const games = model.getGames();
   console.log(req.sessionID);
   res.status(200).json({ list: games });
 });
 
-router.get("/createGame", (req, res) => {
+router.get('/createGame', (req, res) => {
   // Check if user already created a game in the last X seconds.
-  // Should have a database table that keeps track of sessionID and timestamp of latest game creation.
+  // Should have a database table that keeps track of sessionID 
+  // and timestamp of latest game creation.
   // This should be a post request: play as white/black, time controls etc.
   const gameID = model.createGame();
   res.status(200).send({ gameID });
 });
 
-router.get("/joinGame/:gameID", (req, res) => {
+router.get('/joinGame/:gameID', (req, res) => {
   const { gameID } = req.params;
   const { sessionID } = req;
   console.log(sessionID);
