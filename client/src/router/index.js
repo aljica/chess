9b844bd47e98bba1bc11233a8a5416ffdc732316
aljica <aljica@kth.se>
2021-05-19 11:19:50 +0200

    add login view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import GameView from '../views/Game.vue';
 import LobbyView from '../views/Lobby.vue';
+import RegisterView from '../views/Register.vue';
+import LoginView from '../views/Login.vue';
 // import store from '../store';
 
 Vue.use(VueRouter);
@@ -10,6 +12,8 @@ const routes = [
   { path: '/', redirect: '/lobby' },
   { path: '/lobby', component: LobbyView },
   { path: '/game/:gameID', component: GameView },
+  { path: '/register', component: RegisterView },
+  { path: '/login', component: LoginView },
 ];
 
 const router = new VueRouter({
